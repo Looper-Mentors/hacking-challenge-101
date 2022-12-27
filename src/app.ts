2c297c0c2ed8085db62d8813fef7222318603b0b
xtawfeeq7k <taw.coding@gmail.com>
2022-12-27 19:42:15 +0200

    refactor the code -2

--- src/app.ts
+++ src/app.ts
@@ -2,24 +2,9 @@ import express from "express";
 
 import routes from "./routes";
 
-class App {
-  public server;
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(express.json());
 
-  constructor() {
-    this.server = express();
-
-    this.middlewares();
-    this.routes();
-  }
-
-  middlewares() {
-    this.server.use(express.json());
-  }
-
-  routes() {
-    this.server.use(routes);
-    this.server.use(express.json());
-  }
-}
-
-export default new App().server;
+export default app;
